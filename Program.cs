54e69d1748ffdee513cb09c26cad692bd87b82a7
SNuryahya <stya58@gmail.ac.id>
2020-04-13 15:04:46 +0700

    Responsi MID Semester

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ResponsiPemrograman26191
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker (19112619, "Nuryahya", 9999999);
+            Worker worker2 = new Worker (18192598, "Nanda",   15000000);
+
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            worker.showWorker();
+            worker2.showWorker();
+
+            Console.WriteLine("\n\n Mantap Naik Gajiii");
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            worker.naikGaji();
+            worker2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+
+    
+}
